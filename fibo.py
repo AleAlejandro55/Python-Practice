4e6c374d75fa975733085d7e08ffe2381332ad83
Sebas_Alejandro <sgsebas54@gmail.com>
2020-07-25 11:14:46 -0500

    terminando la busqueda binaria y mejorando el script fibo.py

--- fibo.py
+++ fibo.py
@@ -1,12 +1,12 @@
 def run():
-    contador = int(print(cuantos números quieres? ))
+    contador = int(input("cuantos números fibonacci quieres?: "))
     i = 0
     a , b = 0, 1
     while i < contador:
         a = a + b
         b = a - b
         i += 1
-        print(a)
+        print(f'({i})', a)
 
 if __name__=='__main__':
     run()
